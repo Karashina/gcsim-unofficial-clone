4bce0b183897b8537c10bcf38acd5e5f7404bc93
Karashina <54305614+Karashina@users.noreply.github.com>
2024-08-22 23:21:18 +0900

    5.0 weapons from hoyolab

---
+++ internal/weapons/claymore/earthshaker/earthshaker.go
@@ -0,0 +1,83 @@
+package earthshaker
+
+import (
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attacks"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/info"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/gadget"
+	"github.com/genshinsim/gcsim/pkg/modifier"
+)
+
+func init() {
+	core.RegisterWeaponFunc(keys.EarthShaker, NewWeapon)
+}
+
+const (
+	buffKey = "earthshaker-buff"
+)
+
+type Weapon struct {
+	core   *core.Core
+	char   *character.CharWrapper
+	refine int
+	buffE  []float64
+	Index  int
+}
+
+func (w *Weapon) SetIndex(idx int) { w.Index = idx }
+func (w *Weapon) Init() error      { return nil }
+
+func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) (info.Weapon, error) {
+	w := &Weapon{
+		core:   c,
+		char:   char,
+		refine: p.Refine,
+		buffE:  make([]float64, attributes.EndStatType),
+	}
+
+	addBuff := func(args ...interface{}) bool {
+		if _, ok := args[0].(*gadget.Gadget); ok {
+			return false
+		}
+
+		atk := args[1].(*combat.AttackEvent)
+		// don't proc if dmg not from weapon holder
+		if atk.Info.ActorIndex != char.Index {
+			return false
+		}
+
+		w.char.AddAttackMod(character.AttackMod{
+			Base: modifier.NewBaseWithHitlag(buffKey, -1),
+			Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+				w.buffE[attributes.DmgP] = 0.16 // todo:refine
+				switch atk.Info.AttackTag {
+				case attacks.AttackTagElementalArt:
+					return w.buffE, true
+				case attacks.AttackTagElementalArtHold:
+					return w.buffE, true
+				default:
+					return nil, false
+				}
+			},
+		})
+
+		return false
+	}
+
+	subKey := "earthshaker-" + char.Base.Key.String()
+
+	c.Events.Subscribe(event.OnVaporize, addBuff, subKey)
+	c.Events.Subscribe(event.OnMelt, addBuff, subKey)
+	c.Events.Subscribe(event.OnOverload, addBuff, subKey)
+	c.Events.Subscribe(event.OnSwirlPyro, addBuff, subKey)
+	c.Events.Subscribe(event.OnCrystallizePyro, addBuff, subKey)
+	c.Events.Subscribe(event.OnBurning, addBuff, subKey)
+	c.Events.Subscribe(event.OnBurgeon, addBuff, subKey)
+
+	return w, nil
+}
---
+++ internal/weapons/sword/ezpitzal/ezpitzal.go
@@ -0,0 +1,56 @@
+package ezpitzal
+
+import (
+	"fmt"
+
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/info"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/modifier"
+)
+
+func init() {
+	core.RegisterWeaponFunc(keys.FluteOfEzpitzal, NewWeapon)
+}
+
+type Weapon struct {
+	Index int
+}
+
+func (w *Weapon) SetIndex(idx int) { w.Index = idx }
+func (w *Weapon) Init() error      { return nil }
+
+func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) (info.Weapon, error) {
+	w := &Weapon{}
+	r := p.Refine
+
+	defp := 0.16 + float64(r)*0 //todo: refines
+	val := make([]float64, attributes.EndStatType)
+
+	c.Events.Subscribe(event.OnActionExec, func(args ...interface{}) bool {
+		index := args[0].(int)
+		e := args[1].(action.Action)
+		if c.Player.Active() != index {
+			return false
+		}
+		if e != action.ActionSkill {
+			return false
+		}
+
+		val[attributes.DEFP] = defp
+		char.AddStatMod(character.StatMod{
+			Base:         modifier.NewBase("ezpitzal-defp", 15*60),
+			AffectedStat: attributes.DEFP,
+			Amount: func() ([]float64, bool) {
+				return val, true
+			},
+		})
+		return false
+	}, fmt.Sprintf("ezpitzal-skill-%v", char.Base.Key.String()))
+
+	return w, nil
+}
