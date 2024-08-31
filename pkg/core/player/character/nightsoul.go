58eb94c3f00de8692dca7a1d4ae88ef4034052a4
Karashina <54305614+Karashina@users.noreply.github.com>
2024-09-01 07:46:54 +0900

    Nightsoul bugfix

--- pkg/core/player/character/nightsoul.go
+++ pkg/core/player/character/nightsoul.go
@@ -11,12 +11,12 @@ func (c *CharWrapper) ConsumeNightsoul(amt float64) {
 	}
 	preNightsoul := c.NightsoulPoint
 	c.events.Emit(event.OnNightsoulChange, c.Index, -1*amt, "Nightsoul-Drain")
+	c.NightsoulPoint -= amt
 	c.log.NewEvent("draining nightsoul", glog.LogEnergyEvent, c.Index).
 		Write("pre_drain", preNightsoul).
 		Write("post_drain", c.NightsoulPoint).
 		Write("source", c.Base.Key.String()+"-nightsoul-drain").
 		Write("max_nightsoul", c.NightsoulPointMax)
-	c.NightsoulPoint -= amt
 }
 
 func (c *CharWrapper) AddNightsoul(src string, amt float64) {
