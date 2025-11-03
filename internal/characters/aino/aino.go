37166b48edfd93a7e4d07cefe6b35c8dd6d88039
Karashina <54305614+Karashina@users.noreply.github.com>
2025-11-03 21:02:24 +0900

    Adapt aino files to upstream info/Point and Index()

---
+++ internal/characters/aino/aino.go
@@ -0,0 +1,53 @@
+package aino
+
+import (
+	tmpl "github.com/genshinsim/gcsim/internal/template/character"
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/info"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/model"
+)
+
+func init() {
+	core.RegisterCharFunc(keys.Aino, NewChar)
+}
+
+type char struct {
+	*tmpl.Character
+	burstSrc int
+	c2IcdKey string
+}
+
+func NewChar(s *core.Core, w *character.CharWrapper, _ info.CharacterProfile) error {
+	c := char{}
+	c.Character = tmpl.NewWithWrapper(s, w)
+	c.SkillCon = 5
+	c.BurstCon = 3
+
+	c.EnergyMax = 50
+	c.NormalHitNum = normalHitNum
+
+	c.c2IcdKey = "aino-c2-icd"
+
+	w.Character = &c
+
+	return nil
+}
+
+func (c *char) Init() error {
+	// mark this character as a potential moonsign holder for team initialization
+	c.AddStatus("moonsignKey", -1, false)
+	c.c1()
+	c.c2()
+	c.c4()
+	c.c6()
+	return nil
+}
+
+func (c *char) AnimationStartDelay(k model.AnimationDelayKey) int {
+	if k == model.AnimationXingqiuN0StartDelay {
+		return 11
+	}
+	return c.Character.AnimationStartDelay(k)
+}
---
+++ internal/characters/aino/burst.go
@@ -0,0 +1,90 @@
+package aino
+
+import (
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attacks"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+)
+
+var burstFrames []int
+
+const (
+	burstDuration       = 14 * 60 // 14 seconds
+	burstTickInterval   = 90      // 90 frames (default)
+	burstTickIntervalA1 = 42      // 42 frames (when Moonsign is Ascendant)
+	burstKey            = "aino-burst"
+)
+
+func init() {
+	burstFrames = frames.InitAbilSlice(62)
+}
+
+func (c *char) Burst(p map[string]int) (action.Info, error) {
+	c.burstSrc = c.Core.F
+
+	// Determine interval based on A1 passive
+	interval := burstTickInterval
+	radius := 3.0
+	if c.MoonsignAscendant {
+		interval = burstTickIntervalA1
+		radius = 5.0
+	}
+
+	c.QueueCharTask(func() {
+		// Schedule water ball attacks
+		numTicks := burstDuration / interval
+		for i := 0; i < numTicks; i++ {
+			c.Core.Tasks.Add(c.burstTick(c.burstSrc, radius), interval*i)
+		}
+	}, 120)
+
+	c.AddStatus(burstKey, burstDuration, false)
+
+	c.SetCD(action.ActionBurst, 13.5*60) // 13.5s cooldown
+	c.ConsumeEnergy(7)
+
+	return action.Info{
+		Frames:          frames.NewAbilFunc(burstFrames),
+		AnimationLength: burstFrames[action.InvalidAction],
+		CanQueueAfter:   burstFrames[action.ActionDash],
+		State:           action.BurstState,
+	}, nil
+}
+
+func (c *char) burstTick(src int, radius float64) func() {
+	return func() {
+		if src != c.burstSrc {
+			return
+		}
+		if !c.StatusIsActive(burstKey) {
+			return
+		}
+
+		ai := info.AttackInfo{
+			ActorIndex: c.Index(),
+			Abil:       "Precision Hydronic Cooler (Water Ball)",
+			AttackTag:  attacks.AttackTagElementalBurst,
+			ICDTag:     attacks.ICDTagElementalBurst,
+			ICDGroup:   attacks.ICDGroupDefault,
+			StrikeType: attacks.StrikeTypeDefault,
+			Element:    attributes.Hydro,
+			Durability: 25,
+			Mult:       burst[c.TalentLvlBurst()],
+		}
+		if c.MoonsignAscendant {
+			ai.ICDGroup = attacks.ICDGroupAinoBurstEnhanced
+		}
+		if c.Base.Ascension >= 4 {
+			ai.FlatDmg += 0.5 * c.Stat(attributes.EM)
+		}
+
+		c.Core.QueueAttack(
+			ai,
+			combat.NewCircleHitOnTarget(c.Core.Combat.PrimaryTarget(), nil, radius),
+			0,
+			0,
+		)
+	}
+}
