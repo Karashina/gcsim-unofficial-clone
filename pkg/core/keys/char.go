38c7bc91b9a9e1fd8282b8e0c6e069a5aa102e4c
Karashina <54305614+Karashina@users.noreply.github.com>
2025-11-03 21:06:58 +0900

    Update keys char list from main

--- pkg/core/keys/char.go
+++ pkg/core/keys/char.go
@@ -37,11 +37,8 @@ func (c Char) Pretty() string {
 	return charPrettyName[c]
 }
 
-const (
-	ChildePassive = "childe-talent-passive"
-	SkirkPassive  = "skirk-talent-passive"
-)
-
+const ChildePassive = "childe-talent-passive"
+const SkirkPassive = "skirk-talent-passive"
 const (
 	NoChar Char = iota
 	AetherAnemo
